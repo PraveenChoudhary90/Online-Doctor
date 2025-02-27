6249a43f9b9733043c7d36a3683db673b6a0b264
praveen choudhary <pc852180@gmail.com>
2025-02-27 17:47:08 +0530

    Online doctor appoinment

--- Client/src/config.jsx
+++ Client/src/config.jsx
@@ -1,6 +1,6 @@
 // https://online-doctor-nqnl.onrender.com
 
 
-const BASE_URL = "https://online-doctor-nqnl.onrender.com"
+const BASE_URL = "https://online-doctor-3.onrender.com"
 
 export default BASE_URL;
