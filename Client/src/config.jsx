9f852d3671ee59953d76ae48878b291e39e0f2e3
praveen choudhary <pc852180@gmail.com>
2025-02-27 23:18:46 +0530

    doctor online appoinment system

--- Client/src/config.jsx
+++ Client/src/config.jsx
@@ -1,7 +1,7 @@
 // https://online-doctor-nqnl.onrender.com
 
 
-// const BASE_URL = "https://online-doctor-6.onrender.com"
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://online-doctor-3.onrender.com"
+// const BASE_URL = "http://localhost:8000";
 
 export default BASE_URL;
